5f213e9c12949a50d8a26777102ebc2acdc3ba7c
ramy670 <49255215+ramy670@users.noreply.github.com>
2022-12-01 03:48:30 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
